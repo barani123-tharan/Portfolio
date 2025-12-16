38c9db178b6affdcec9763a0362b5f31262f1aea
barani123-tharan <baranitharantamilselvam@gmail.com>
2025-12-16 12:02:40 +0530

    fix: Final resolution of all remaining config file conflicts

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-=======
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
->>>>>>> 736c236537ee1e19d2ce090ebfab12a833b0c9bb
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -15,22 +14,4 @@ export default {
     },
   },
   plugins: [],
-=======
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        dark: "#0f172a", // This makes 'bg-dark' work
-        primary: "#6366f1",
-        secondary: "#ec4899",
-      },
-    },
-  },
-  plugins: [],
->>>>>>> 736c236537ee1e19d2ce090ebfab12a833b0c9bb
 }
